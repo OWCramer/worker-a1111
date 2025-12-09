87bf6292ef6fb54304e46b078a54681f934639e4
Owen Cramer <owencramer1218@gmail.com>
2025-12-09 16:28:37 -0500

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -7,30 +7,28 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 export PYTHONUNBUFFERED=true
 
-declare -a ARGS
-
-ARGS+=(--xformers)
-ARGS+=(--no-half-vae)
-ARGS+=(--skip-python-version-check)
-ARGS+=(--skip-torch-cuda-test)
-ARGS+=(--skip-install)
-
 if [ -f "/workspace/models/model.safetensor" ]; then
+    declare -a ARGS
+
+    ARGS+=(--xformers)
+    ARGS+=(--no-half-vae)
+    ARGS+=(--skip-python-version-check)
+    ARGS+=(--skip-torch-cuda-test)
+    ARGS+=(--skip-install)
     ARGS+=(--ckpt "/workspace/models/model.safetensor")
+    ARGS+=(--opt-sdp-attention)
+    ARGS+=(--disable-safe-unpickle)
+    ARGS+=(--port 3000)
+    ARGS+=(--api)
+    ARGS+=(--nowebui)
+    ARGS+=(--skip-version-check)
+    ARGS+=(--no-hashing)
+    ARGS+=(--no-download-sd-model)
+
+    python /stable-diffusion-webui/webui.py "${ARGS[@]}" &
 else
-    echo "WARN: Model not found at /workspace/models/model.safetensor. Starting without --ckpt."
+    echo "WARN: Model not found at /workspace/models/model.safetensor. Skipping WebUI start."
 fi
-
-ARGS+=(--opt-sdp-attention)
-ARGS+=(--disable-safe-unpickle)
-ARGS+=(--port 3000)
-ARGS+=(--api)
-ARGS+=(--nowebui)
-ARGS+=(--skip-version-check)
-ARGS+=(--no-hashing)
-ARGS+=(--no-download-sd-model)
-
-python /stable-diffusion-webui/webui.py "${ARGS[@]}" &
 
 echo "Starting RunPod Handler"
 python -u /handler.py
