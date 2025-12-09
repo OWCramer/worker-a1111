73757604581bdce1facb73b6986154eb325b0c46
Owen Cramer <owencramer1218@gmail.com>
2025-12-09 15:35:58 -0500

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -12,7 +12,7 @@ python /stable-diffusion-webui/webui.py \
   --skip-python-version-check \
   --skip-torch-cuda-test \
   --skip-install \
-  --ckpt /workspace/models/model.safetensor \
+  --ckpt /runpod-volume/models/model.safetensor \
   --opt-sdp-attention \
   --disable-safe-unpickle \
   --port 3000 \
