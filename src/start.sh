3d3bd6329e6e0418e253991d7c62fec860355857
Owen Cramer <owencramer1218@gmail.com>
2025-12-09 16:16:04 -0500

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -7,28 +7,30 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 export PYTHONUNBUFFERED=true
 
+declare -a ARGS
+
+ARGS+=(--xformers)
+ARGS+=(--no-half-vae)
+ARGS+=(--skip-python-version-check)
+ARGS+=(--skip-torch-cuda-test)
+ARGS+=(--skip-install)
+
 if [ -f "/workspace/models/model.safetensor" ]; then
-    CKPT_ARG="--ckpt /workspace/models/model.safetensor"
+    ARGS+=(--ckpt "/workspace/models/model.safetensor")
 else
     echo "WARN: Model not found at /workspace/models/model.safetensor. Starting without --ckpt."
-    CKPT_ARG=""
 fi
 
-python /stable-diffusion-webui/webui.py \
-  --xformers \
-  --no-half-vae \
-  --skip-python-version-check \
-  --skip-torch-cuda-test \
-  --skip-install \
-  "$CKPT_ARG" \
-  --opt-sdp-attention \
-  --disable-safe-unpickle \
-  --port 3000 \
-  --api \
-  --nowebui \
-  --skip-version-check \
-  --no-hashing \
-  --no-download-sd-model &
+ARGS+=(--opt-sdp-attention)
+ARGS+=(--disable-safe-unpickle)
+ARGS+=(--port 3000)
+ARGS+=(--api)
+ARGS+=(--nowebui)
+ARGS+=(--skip-version-check)
+ARGS+=(--no-hashing)
+ARGS+=(--no-download-sd-model)
+
+python /stable-diffusion-webui/webui.py "${ARGS[@]}" &
 
 echo "Starting RunPod Handler"
 python -u /handler.py
