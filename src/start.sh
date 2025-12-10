fbc8e049901b45c0f91270f447e92307779123ce
Owen Cramer <owencramer1218@gmail.com>
2025-12-10 10:46:40 -0500

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -7,25 +7,26 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 export PYTHONUNBUFFERED=true
 
-    declare -a ARGS
+declare -a ARGS
 
-    ARGS+=(--xformers)
-    ARGS+=(--no-half-vae)
-    ARGS+=(--skip-python-version-check)
-    ARGS+=(--skip-torch-cuda-test)
-    ARGS+=(--skip-install)
-    ARGS+=(--ckpt "/runpod-volume/models/model.safetensor")
-    ARGS+=(--opt-sdp-attention)
-    ARGS+=(--disable-safe-unpickle)
-    ARGS+=(--port 3000)
-    ARGS+=(--api)
-    ARGS+=(--nowebui)
-    ARGS+=(--skip-version-check)
-    ARGS+=(--no-hashing)
-    ARGS+=(--no-download-sd-model)
+ARGS+=(--xformers)
+ARGS+=(--no-half-vae)
+ARGS+=(--skip-python-version-check)
+ARGS+=(--skip-torch-cuda-test)
+ARGS+=(--skip-install)
+ARGS+=(--ckpt "/runpod-volume/models/model.safetensor")
+ARGS+=(--opt-sdp-attention)
+ARGS+=(--disable-safe-unpickle)
+ARGS+=(--port 3000)
+ARGS+=(--api)
+ARGS+=(--nowebui)
+ARGS+=(--skip-version-check)
+ARGS+=(--no-hashing)
+ARGS+=(--no-download-sd-model)
 
-    python /stable-diffusion-webui/webui.py "${ARGS[@]}" &
+python /stable-diffusion-webui/webui.py "${ARGS[@]}" &
 
+ls
 
 echo "Starting RunPod Handler"
 python -u /handler.py
